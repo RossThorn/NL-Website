6c8abae7a2277ecb32e9085704bfa942a5148a48
Ross Thorn <rthorn@wisc.edu>
2019-11-28 20:46:50 -0600

    Added mobile nav bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Need to convert to include JQUERY
-var navbar = document.getElementById("menu-bar");
+var navbar = document.getElementById("desktop-nav");
 var buttons = document.getElementById("middle-wrapper").children;
+var mobileNav = document.getElementById("mobile-nav");
 
 // Content for watchers
 var header = document.getElementById("header");
@@ -11,6 +12,7 @@ var merchandise = document.getElementById("merchandise");
 var contact = document.getElementById("contact");
 
 var logo = document.getElementById("nav-logo");
+var mobileLogo = document.getElementById("mobile-logo");
 
 // var content = document.getElementById("media");
 
@@ -24,6 +26,8 @@ var contactWatcher = scrollMonitor.create(contact);
 
 var didScroll = false;
 
+var menuShowing = false;
+
 window.onscroll = checkHeader;
 
 function checkHeader() {
@@ -32,20 +36,30 @@ function checkHeader() {
 
 function main(){
 
-  if (headerWatcher.isAboveViewport){
+  headerWatcher.exitViewport(function(){
     barStick();
-  }
-
-  aboutWatcher.fullyEnterViewport(function(){
-    barStick();
+    if (window.innerWidth < 800){
+      mobileLogo.style.opacity = 1;
+    }
   });
+
+
+
+  // aboutWatcher.fullyEnterViewport(function(){
+  //   barStick();
+  //   if (window.innerWidth < 400){
+  //     mobileLogo.style.opacity = 1;
+  //   }
+  // });
 
   headerWatcher.enterViewport(function(){
     barUnstick();
+    if (window.innerWidth < 800){
+      mobileLogo.style.opacity = 0;
+    }
   });
 
   document.getElementById("cover").style.opacity = 1;
-  console.log(window.innerWidth);
     if (window.innerWidth < 400){
       // console.log(document.getElementsByClassName("lightwidget-widget")[0].src);
       document.getElementsByClassName("lightwidget-widget")[0].src = "http://lightwidget.com/widgets/2c470acc61a65905819dc1b826c5a12a.html";
@@ -53,7 +67,7 @@ function main(){
 
 }
 function barStick() {
-    logo.style.opacity = "1";
+    logo.style.opacity = 1;
 
     navbar.classList.add("sticky");
     navbar.classList.remove("non-stick");
@@ -68,6 +82,16 @@ function barStick() {
     navbar.style.backgroundColor = "#1f1f1f";
     // about.style.paddingTop = "0";
 
+}
+
+function dropdown(){
+  if (menuShowing == false){
+  document.getElementById("dropdown-content").style.display = "block";
+  menuShowing = true;
+} else {
+  document.getElementById("dropdown-content").style.display = "none";
+  menuShowing = false;
+}
 }
 
 
