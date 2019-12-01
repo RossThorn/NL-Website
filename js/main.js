fad47247e3fc41a18d89a0df628525859d1d4312
Ross Thorn <rthorn@wisc.edu>
2019-12-01 10:10:01 -0600

    Changed band image and formatted "about" section

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var navbar = document.getElementById("desktop-nav");
 var buttons = document.getElementById("middle-wrapper").children;
 var mobileNav = document.getElementById("mobile-nav");
 
+var content = document.getElementById("content");
+
 // Content for watchers
 var header = document.getElementById("header");
 var about = document.getElementById("about");
@@ -10,6 +12,7 @@ var music = document.getElementById("music");
 var gallery = document.getElementById("gallery");
 var merchandise = document.getElementById("merchandise");
 var contact = document.getElementById("contact");
+// var contentItems = document.getElementsByClassName("content-section")
 
 var logo = document.getElementById("nav-logo");
 var mobileLogo = document.getElementById("mobile-logo");
@@ -40,17 +43,17 @@ function main(){
     barStick();
     if (window.innerWidth < 800){
       mobileLogo.style.opacity = 1;
+    } else {
+          about.style.paddingTop = "20vh";
+        if (document.getElementsByClassName("about-overlay").length > 0) {
+          document.getElementsByClassName("about-overlay")[0].style.marginTop = "10vh";
+        }
     }
+    if (document.getElementsByClassName("about-overlay").length > 0) {
+      document.getElementsByClassName("about-overlay")[0].style.opacity = 1;
+    }
+
   });
-
-
-
-  // aboutWatcher.fullyEnterViewport(function(){
-  //   barStick();
-  //   if (window.innerWidth < 400){
-  //     mobileLogo.style.opacity = 1;
-  //   }
-  // });
 
   headerWatcher.enterViewport(function(){
     barUnstick();
@@ -60,27 +63,23 @@ function main(){
   });
 
   document.getElementById("cover").style.opacity = 1;
-    if (window.innerWidth < 400){
-      // console.log(document.getElementsByClassName("lightwidget-widget")[0].src);
-      document.getElementsByClassName("lightwidget-widget")[0].src = "http://lightwidget.com/widgets/2c470acc61a65905819dc1b826c5a12a.html";
-    }
+    // if (window.innerWidth < 400){
+    //   document.getElementsByClassName("lightwidget-widget")[0].src = "http://lightwidget.com/widgets/2c470acc61a65905819dc1b826c5a12a.html";
+    // }
 
 }
 function barStick() {
     logo.style.opacity = 1;
-
     navbar.classList.add("sticky");
     navbar.classList.remove("non-stick");
-    // about.style.paddingTop = "10vh";
-    // navbar.style.backgroundColor = "#333333";
   }
 
   function barUnstick(){
     logo.style.opacity = "0";
+    about.style.paddingTop = "10vh";
     navbar.classList.remove("sticky");
     navbar.classList.add("non-stick");
     navbar.style.backgroundColor = "#1f1f1f";
-    // about.style.paddingTop = "0";
 
 }
 
